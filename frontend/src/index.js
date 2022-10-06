f635f640fd42bdd72d7137c855151ad1956c36cc
Connor <kor.connor@gmail.com>
2022-10-07 05:38:58 +0900

    기타 리스트 뿌리기

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,20 +3,15 @@ import ReactDOM from "react-dom"
 
 class Card extends React.Component {
   render() {
-    const img = "img/strings.png"
-    const imgalt = "string"
-    const desc = "A very authentic and beautiful instrument!!"
-    const price = 100
-    const productName = "Strings"
-
     return (
+
       <div className="col-md-6 col-lg-4 d-flex align-items-stretch">
         <div className="card mb-3">
-          <img className="card-img-top" src={img} alt={imgalt}/>
+          <img className="card-img-top" src={this.props.img} alt={this.props.imgalt}/>
           <div className="card-body">
-            <h4 className="card-title">{productName}</h4>
-              Price: <strong>{price}</strong>
-            <p className="card-text">{desc}</p>
+            <h4 className="card-title">{this.props.productName}</h4>
+              Price: <strong>{this.props.price}</strong>
+            <p className="card-text">{this.props.desc}</p>
             <a href="#" className="btn btn-primary">Buy</a>
           </div>
         </div>
@@ -25,4 +20,16 @@ class Card extends React.Component {
   }
 }
 
-ReactDOM.render(<Card/>, document.getElementById('root'))
+class CardContainer extends React.Component {
+  render() {
+    return(
+      
+      <div>
+        <Card key='1' img="img/strings.png" alt="strings" productName="Strings" price='100.0' desc="A very authentic and beautiful instrument!!"/>
+        <Card key='2' img="img/redguitar.jpeg" alt="redg" productName="Red Guitar" price='299.0' desc="A really cool red guitar that can produce super cool music!!"/>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<CardContainer/>, document.getElementById('root'))
