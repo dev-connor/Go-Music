7f4d9148e2301661f7951291222ec444185a2da5
Connor <kor.connor@gmail.com>
2022-10-10 21:04:46 +0900

    go get -u github.com/gin-gonic/gin

--- backend/src/dblayer/dblayer.go
+++ backend/src/dblayer/dblayer.go
@@ -1,5 +1,15 @@
 package dblayer
 
+import "backend/src/src/models"
+
 type DBLayer interface {
-	GetAllProducts() ([]model.Product, error)
+	GetAllProducts() ([]models.Product, error)
+	GetPromos() ([]models.Product, error)
+	GetCustomerByName() (models.Customer, error)
+	GetCustomerByID(int) (models.Product, error)
+	GetProduct(uint) (models.Product, error)
+	AddUser(models.Customer) (models.Customer, error)
+	SignInUser(username, password string) (models.Customer, error)
+	SignOutUserById(int) error
+	GetCustomerOrdersByID(int) ([]models.Order, error)
 }
