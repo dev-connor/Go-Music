e6f5e57f3bbc0df4c335835d37cdc947668f8790
Connor <kor.connor@gmail.com>
2022-10-10 21:36:30 +0900

    핸들러 구현 ~주문내역 조회 까지

---
+++ backend/src/rest/handler/handler.go
@@ -0,0 +1,113 @@
+package handler
+
+import (
+	"backend/src/src/dblayer"
+	"backend/src/src/models"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
+
+type HandlerInterface interface {
+	GetProducts(c *gin.Context)
+	GetPromos(c *gin.Context)
+	AddUser(c *gin.Context)
+	SignIn(c *gin.Context)
+	SignOut(c *gin.Context)
+	GetOrders(c *gin.Context)
+	Charge(c *gin.Context)
+}
+
+type Handler struct {
+	db dblayer.DBLayer
+}
+
+func NewHandler() (*Handler, error) {
+	// Handler 객체에 대한 포인터 생성
+	return new(Handler), nil
+}
+
+func (h *Handler) GetProducts(c *gin.Context) {
+	if h.db == nil {
+		return
+	}
+	products, err := h.db.GetAllProducts()
+	if err != nil {
+		/*
+			첫 번째 매개변수는 HTTP 상태 코드, 두 번째는 응답의 바디
+		*/
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, products)
+}
+
+func (h *Handler) GetPromos(c *gin.Context) {
+	if h.db == nil {
+		return
+	}
+	promos, err := h.db.GetPromos()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, promos)
+}
+
+func (h *Handler) SignIn(c *gin.Context) {
+	if h.db == nil {
+		return
+	}
+	var customer models.Customer
+	err := c.ShouldBindJSON(&customer)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	customer, err = h.db.AddUser(customer)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, customer)
+}
+
+func (h *Handler) SignOut(c *gin.Context) {
+	if h.db == nil {
+		return
+	}
+	p := c.Param("id")
+	// p 는 문자형. 정수형으로 변환해야 함
+	id, err := strconv.Atoi(p)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	err = h.db.SignOutUserById(id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+}
+
+func (h *Handler) GetOrders(c *gin.Context) {
+	if h.db == nil {
+		return
+	}
+	// id 매개변수 추출
+	p := c.Param("id")
+
+	id, err := strconv.Atoi(p)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// 데이터베이스 레이어 메서드 호출과 주문내역 조회
+	orders, err := h.db.GetCustomerOrdersByID(id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, orders)
+}
--- backend/src/rest/rest.go
+++ backend/src/rest/rest.go
@@ -1,1 +1,56 @@
 package rest
+
+import "github.com/gin-gonic/gin"
+
+func RunAPI(address string) error {
+	r := gin.Default()
+	r.GET("/relativepath/to/url", func(c *gin.Context) {
+		// 로직 구현
+	})
+
+	// 상품 목록
+	r.GET("/products", func(c *gin.Context) {
+		// 클라이언트에게 상품 목록 반환
+
+	})
+
+	// 프로모션 목록
+	r.GET("/promos", func(c *gin.Context) {
+
+	})
+
+	// 사용자 로그인 POST 요청
+	r.POST("/users/signin", func(c *gin.Context) {
+		// 사용자 로그인
+
+	})
+
+	// 사용자 추가 POST 요청
+	r.POST("/users", func(c *gin.Context) {
+		// 사용자 추가
+
+	})
+
+	// 사용자 로그아웃 POST 요청
+	/*
+		아래 경로는 사용자 ID 를 포함한다. ID 는 사용자마다 고유한 값이기 때문에
+		와일드카드 (*) 를 사용한다. ':id' 는 변수 id 를 의미한다.
+	*/
+	r.POST("/user/:id/signout", func(c *gin.Context) {
+		// 해당 ID 의 사용자 로그아웃
+
+	})
+
+	// 구매 목록 조회
+	r.GET("/user/:id/orders", func(c *gin.Context) {
+		// 해당 ID 의 사용자의 주문내역 조회
+
+	})
+
+	// 결제 POST 요청
+	r.POST("/users/charge", func(c *gin.Context) {
+		// 신용카드 결제 처리
+
+	})
+
+}
