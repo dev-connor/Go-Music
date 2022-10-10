78eaf8021b71b6623b29a0f766829701f4fceaee
Connor <kor.connor@gmail.com>
2022-10-11 00:03:14 +0900

    GORM 배우기

--- backend/src/models/models.go
+++ backend/src/models/models.go
@@ -1,14 +1,22 @@
 package models
 
-import "time"
+import (
+	"github.com/jinzhu/gorm"
+	"time"
+)
 
 type Product struct {
+	gorm.Model
 	Image       string  `json:"img"`
-	ImagAlt     string  `json:"imgalt"`
+	ImagAlt     string  `json:"imgalt" gorm:"column:imgalt"`
 	Price       float64 `json:"price"`
-	Promotion   float64 `json:"promotion"`
-	ProductName string  `json:"productname"`
-	Description string  `json:"desc"`
+	Promotion   float64 `json:"promotion"` // sql.NullFloat64
+	ProductName string  `json:"productname" gorm:"column:productname"`
+	Description string
+}
+
+func (Product) TableName() string {
+	return "products"
 }
 
 type Customer struct {
@@ -19,10 +27,15 @@ type Customer struct {
 }
 
 type Order struct {
+	gorm.Model
 	Product
 	Customer
-	CustomerID   int       `json:"customer_id"`
-	ProductID    int       `json:"product_id"`
-	Price        float64   `json:"sell_price"`
-	PurchaseDate time.Time `json:"purchase_date"`
+	CustomerID   int       `gorm:"column:customer_id"`
+	ProductID    int       `gorm:"column:product_id"`
+	Price        float64   `json:"sell_price" gorm:"column:price"`
+	PurchaseDate time.Time `json:"purchase_date" gorm:"column:purchase_date"`
+}
+
+func (Order) TableName() string {
+	return "orders"
 }
