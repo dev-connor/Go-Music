88c26a844494933337cc2222bdce8f5478e9efe3
Connor <kor.connor@gmail.com>
2022-10-10 22:01:29 +0900

    handler.go 파일을 rest 폴더로 이동

--- backend/src/rest/handler.go
+++ backend/src/rest/handler.go
@@ -1,4 +1,4 @@
-package handler
+package rest
 
 import (
 	"backend/src/src/dblayer"
--- backend/src/rest/rest.go
+++ backend/src/rest/rest.go
@@ -1,28 +1,33 @@
 package rest
 
 import (
-	"backend/src/src/rest/handler"
 	"github.com/gin-gonic/gin"
 )
 
 func RunAPI(address string) error {
 	r := gin.Default()
 
-	h, _ := handler.NewHandler()
+	h, _ := NewHandler()
 
 	r.GET("/products", h.GetProducts)
 
 	r.GET("/promos", h.GetPromos)
 
-	r.POST("/users/signin", h.SignIn)
-
-	r.POST("/users", h.AddUser)
-
-	r.POST("/user/:id/signout", h.SignOut)
-
-	r.GET("/user/:id/orders", h.GetOrders)
-
-	r.POST("/users/charge", h.Charge)
+	userGroup := r.Group("/user")
+	{
+		userGroup.POST("/:id/signout", h.SignOut)
+		userGroup.GET("/:id/orders", h.GetOrders)
+	}
+	usersGroup := r.Group("/users")
+	{
+		usersGroup.POST("/charge", h.Charge)
+		usersGroup.POST("/signin", h.SignIn)
+		usersGroup.POST("", h.AddUser)
+	}
 
 	return r.Run(address)
+}
+
+func RunAPIWithHandler(address string, h HandlerInterface) {
+
 }
