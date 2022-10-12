67158e074f8c6e229e0353d8873c108da98e6b02
Connor <kor.connor@gmail.com>
2022-10-12 21:22:18 +0900

    데이터베이스 레이어 구현

--- backend/src/dblayer/orm.go
+++ backend/src/dblayer/orm.go
@@ -2,6 +2,7 @@ package dblayer
 
 import (
 	"backend/src/src/models"
+	"errors"
 	"github.com/jinzhu/gorm"
 )
 
@@ -26,4 +27,44 @@ func (db *DBORM) GetPromos() (products models.Product, err error) {
 
 func (db *DBORM) GetCustomerByName(firstname, lastname string) (customer models.Customer, err error) {
 	return customer, db.Where(&models.Customer{FirstName: firstname, LastName: lastname}).Find(&customer).Error
+}
+
+func (db *DBORM) GetCustomerByID(id int) (customer models.Customer, err error) {
+	return customer, db.First(&customer, id).Error
+}
+
+func (db *DBORM) GetProduct(id int) (product models.Product, err error) {
+	return product, db.First(&product, id).Error
+}
+
+func (db *DBORM) AddUser(customer models.Customer) (models.Customer, error) {
+	hashPassword(&customer.Pass)
+	customer.LoggedIn = true
+	return customer, db.Create(&customer).Error
+}
+
+func (db *DBORM) SignInUser(email, pass string) (customer models.Customer, err error) {
+	if !checkPassword(pass) {
+		return customer, errors.New("Invalid password")
+	}
+	// 사용자 행을 나타내는 *gorm.DB 타입
+	result := db.Table("Customers").Where(&models.Customer{Email: email})
+	// loggedin 필드 업데이트
+	err = result.Update("loggedin", 1).Error
+	if err != nil {
+		return customer, err
+	}
+	// 사용자 행 반환
+	return customer, result.Find(&customer).Error
+}
+
+func (db *DBORM) SignOutUserById(id int) error {
+	// ID 에 해당하는 사용자 구조체 생성
+	customer := models.Customer{
+		Model: gorm.Model{
+			ID: uint(id),
+		},
+	}
+	// 사용자의 상태를 로그아웃 상태로 업데이트한다.
+	return db.Table("Customers").Where(&customer).Update("loggedin", 0).Error
 }
