870de149dfb458fd1dc94738a6ad7ca1b94c4067
Connor <kor.connor@gmail.com>
2022-10-12 22:29:57 +0900

    패스워드 비교 추가

--- backend/src/dblayer/orm.go
+++ backend/src/dblayer/orm.go
@@ -63,11 +63,20 @@ func hashPassword(s *string) error {
 }
 
 func (db *DBORM) SignInUser(email, pass string) (customer models.Customer, err error) {
-	if !checkPassword(pass) {
-		return customer, errors.New("Invalid password")
-	}
-	// 사용자 행을 나타내는 *gorm.DB 타입
+	// 사용자 행을 나타내는 *gorm.DB 타입 할당
 	result := db.Table("Customers").Where(&models.Customer{Email: email})
+	// 입력된 이메일로 사용자 정보 조회
+	err = result.First(&customer).Error
+	if err != nil {
+		return customer, err
+	}
+	// 패스워드 문자열과 해시 값 비교
+	if !checkPassword(customer.Pass, pass) {
+		// 불일치 시 에러 반환
+		return customer, ErrINVALIDPASSWORD
+	}
+	// 공유되지 않도록 패스워드 문자열은 지운다.
+	customer.Pass = ""
 	// loggedin 필드 업데이트
 	err = result.Update("loggedin", 1).Error
 	if err != nil {
@@ -75,6 +84,13 @@ func (db *DBORM) SignInUser(email, pass string) (customer models.Customer, err e
 	}
 	// 사용자 행 반환
 	return customer, result.Find(&customer).Error
+}
+
+func checkPassword(existingHash, incomingPass string) bool {
+	// 해시와 패스워드 문자열이 일치하지 않으면
+	// 아래 메서드는 에러를 반환한다.
+	return bcrypt.CompareHashAndPassword([]byte(existingHash),
+		[]byte(incomingPass)) == nil
 }
 
 func (db *DBORM) SignOutUserById(id int) error {
