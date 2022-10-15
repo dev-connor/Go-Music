fc2c25c43b1a2eb8ed47720c095a7e593dbd3707
Connor <kor.connor@gmail.com>
2022-10-15 12:50:54 +0900

    dblayer.go 복붙

--- backend/src/dblayer/dblayer.go
+++ backend/src/dblayer/dblayer.go
@@ -8,9 +8,9 @@ import (
 type DBLayer interface {
 	GetAllProducts() ([]models.Product, error)
 	GetPromos() ([]models.Product, error)
-	GetCustomerByName() (models.Customer, error)
-	GetCustomerByID(int) (models.Product, error)
-	GetProduct(uint) (models.Product, error)
+	GetCustomerByName(string, string) (models.Customer, error)
+	GetCustomerByID(int) (models.Customer, error)
+	GetProduct(int) (models.Product, error)
 	AddUser(models.Customer) (models.Customer, error)
 	SignInUser(username, password string) (models.Customer, error)
 	SignOutUserById(int) error
@@ -19,5 +19,13 @@ type DBLayer interface {
 	GetCreditCardCID(int) (string, error)
 	SaveCreditCardForCustomer(int, string) error
 }
+
+/*
+Passwords:
+mal:123
+john:1111
+jayne:123
+River:abc
+*/
 
 var ErrINVALIDPASSWORD = errors.New("Invalid password")
